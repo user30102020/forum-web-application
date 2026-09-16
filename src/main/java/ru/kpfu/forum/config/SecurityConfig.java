59ccd59fffabb93a88ebcaab87259dce60bad06e
user30102020 <bril.maks19.04@mail.ru>
2026-09-16 19:22:13 +0300

    fix: deleted extra comma in SecurityConfig

--- src/main/java/ru/kpfu/forum/config/SecurityConfig.java
+++ src/main/java/ru/kpfu/forum/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package ru.kpfu.forum.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,8 +18,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(
-            HttpSecurity http,
-            AuthenticationConfiguration authenticationConfiguration
+            HttpSecurity http
     ) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
